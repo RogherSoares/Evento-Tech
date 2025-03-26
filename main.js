e37603d0ed39aab95302ca6e38472f973b2c2a01
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-03-26 19:15:02 -0300

    atualizando o código

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-/* Menu Hamburguer*/
 const menuHamburguer = document.querySelector(".menuHamburguer"); 
-
 const menu = document.querySelector(".menu");
+const main = document.querySelector("main");
 
-menuHamburguer.addEventListener("click", () => menu.classList.toggle("active"));
+menuHamburguer.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    main.classList.toggle("active"); 
+});
+
+    
